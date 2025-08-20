557f7d65f08de15d0e9692cf00640caa7bdb7d86
pavithra-dev2003 <pavethrapavethra588@gmail.com>
2025-08-20 21:08:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
     }
   }
 }
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {}
